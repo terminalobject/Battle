f50a37e37612e03a9fc278bdc69603677f4fe486
terminalobject <doctorboredom@hotmail.com>
2017-06-23 15:42:13 +0100

    refactored removing global variable

--- app.rb
+++ app.rb
@@ -5,38 +5,36 @@ require './lib/game'
 
 class Battle < Sinatra::Base
   enable :sessions
-
+  before do
+    @game = Game.instance
+  end
   get '/' do
     erb(:index)
   end
 
   post '/names' do
     player1 = Player.new(params[:Player_1])
-    # p player1
     player2 = Player.new(params[:Player_2])
-    # p player2
-    $game = Game.new(player1, player2)
-    # p $game.player1.player_name
+    @game = Game.create(player1, player2)
     redirect to '/play'
   end
 
   get '/play' do
-    # p $game.player1.player_name
-    @player1 = $game.player1.player_name
-    @player2 = $game.player2.player_name
-    @p1_health = $game.player1.health
-    @p2_health = $game.player2.health
-    redirect to '/endgame' if $game.player1.dead? || $game.player2.dead?
+    @player1 = @game.player1.player_name
+    @player2 = @game.player2.player_name
+    @p1_health = @game.player1.health
+    @p2_health = @game.player2.health
+    redirect to '/endgame' if @game.player1.dead? || @game.player2.dead?
     erb(:play)
   end
 
   get '/attack' do
-    @player1 = $game.player1.player_name
-    @player2 = $game.player2.player_name
-    $game.attack
-    $game.switch_turn
-    @p1_health = $game.player1.health
-    @p2_health = $game.player2.health
+    @player1 = @game.player1.player_name
+    @player2 = @game.player2.player_name
+    @game.attack
+    @game.switch_turn
+    @p1_health = @game.player1.health
+    @p2_health = @game.player2.health
     erb(:attack)
   end
 
--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,14 @@
 class Game
   attr_reader :player1, :player2, :turn
 
+  def self.create(player1, player2)
+    @game = Game.new(player1, player2)
+  end
+
+  def self.instance
+    @game
+  end
+
   def initialize(player1, player2, turn = [true,false].sample)
     @player1 = player1
     @player2 = player2
